b95bc94126da07ada05a0b7582c5807494d43f46
Gabriel Braithuaite <u20201e889@upc.edu.pe>
2026-02-07 15:23:36 -0500

    feat: add queries for retrieving users by id, username, and all users

---
+++ src/main/java/org/financeapp/iam/domain/model/queries/GetAllUsersQuery.java
@@ -0,0 +1,10 @@
+package org.financeapp.iam.domain.model.queries;
+
+/**
+ * Get all users query
+ * <p>
+ *     This class represents the query to get all the users in the system.
+ * </p>
+ */
+public record GetAllUsersQuery() {
+}

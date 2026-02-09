421d80b93a593a45000b28dae45b286d3c30c370
Gabriel Braithuaite <u20201e889@upc.edu.pe>
2026-02-09 13:42:41 -0500

    feat: add HashingService and TokenService interfaces for password encoding and token management

---
+++ src/main/java/org/financeapp/iam/application/internal/commandservices/UserCommandServiceImpl.java
@@ -0,0 +1,66 @@
+package org.financeapp.iam.application.internal.commandservices;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.financeapp.iam.application.internal.outboundservices.hashing.HashingService;
+import org.financeapp.iam.application.internal.outboundservices.tokens.TokenService;
+import org.financeapp.iam.domain.model.aggregates.User;
+import org.financeapp.iam.domain.model.commands.SignInCommand;
+import org.financeapp.iam.domain.model.commands.SignUpCommand;
+import org.financeapp.iam.domain.services.UserCommandService;
+import org.financeapp.iam.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserCommandServiceImpl implements UserCommandService {
+
+    private final UserRepository userRepository;
+    private final HashingService hashingService;
+    private final TokenService tokenService;
+
+
+    public UserCommandServiceImpl(UserRepository userRepository, HashingService hashingService, TokenService tokenService) {
+        this.userRepository = userRepository;
+        this.hashingService = hashingService;
+        this.tokenService = tokenService;
+
+    }
+
+    /**
+     * Handle the sign-in command
+     * <p>
+     *     This method handles the {@link SignInCommand} command and returns the user and the token.
+     * </p>
+     * @param command the sign-in command containing the username and password
+     * @return and optional containing the user matching the username and the generated token
+     * @throws RuntimeException if the user is not found or the password is invalid
+     */
+    @Override
+    public Optional<ImmutablePair<User, String>> handle(SignInCommand command) {
+        var user = userRepository.findByUsername(command.username());
+        if (user.isEmpty())
+            throw new RuntimeException("User not found");
+        if (!hashingService.matches(command.password(), user.get().getPassword()))
+            throw new RuntimeException("Invalid password");
+        var token = tokenService.generateToken(user.get().getUsername());
+        return Optional.of(ImmutablePair.of(user.get(), token));
+    }
+
+    /**
+     * Handle the sign-up command
+     * <p>
+     *     This method handles the {@link SignUpCommand} command and returns the user.
+     * </p>
+     * @param command the sign-up command containing the username and password
+     * @return the created user
+     */
+    @Override
+    public Optional<User> handle(SignUpCommand command) {
+        if (userRepository.existsByUsername(command.username()))
+            throw new RuntimeException("Username already exists");
+        var user = new User(command.username(), hashingService.encode(command.password()));
+        userRepository.save(user);
+        return userRepository.findByUsername(command.username());
+    }
+}

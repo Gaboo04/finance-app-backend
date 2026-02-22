39836992dcba71ab2db3198f1024b6d7f4000feb
Gabriel Braithuaite <u20201e889@upc.edu.pe>
2026-02-21 20:08:10 -0500

    feat: simplify SignUpCommand and UserResource transformations by removing roles handling

---
+++ src/main/java/org/financeapp/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
@@ -0,0 +1,15 @@
+package org.financeapp.iam.interfaces.interfaces.rest.transform;
+package org.financeapp.iam.interfaces.rest.transform;
+
+import org.financeapp.iam.domain.model.commands.SignUpCommand;
+import org.financeapp.iam.interfaces.rest.resources.SignUpResource;
+
+public class SignUpCommandFromResourceAssembler {
+
+    public static SignUpCommand toCommandFromResource(SignUpResource resource) {
+        return new SignUpCommand(
+                resource.username(),
+                resource.password()
+        );
+    }
+}
---
+++ src/main/java/org/financeapp/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -0,0 +1,15 @@
+package org.financeapp.iam.interfaces.interfaces.rest.transform;
+package org.financeapp.iam.interfaces.rest.transform;
+
+import org.financeapp.iam.domain.model.aggregates.User;
+import org.financeapp.iam.interfaces.rest.resources.UserResource;
+
+public class UserResourceFromEntityAssembler {
+
+    public static UserResource toResourceFromEntity(User user) {
+        return new UserResource(
+                user.getId(),
+                user.getUsername()
+        );
+    }
+}

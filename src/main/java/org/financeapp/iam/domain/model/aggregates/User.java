678872ca558d9560d3bc8e25748a54aac9d8ddf3
Gabriel Braithuaite <u20201e889@upc.edu.pe>
2026-02-21 20:07:15 -0500

    feat: refactor User entity to use Roles enum and improve role management

--- src/main/java/org/financeapp/iam/domain/model/aggregates/User.java
+++ src/main/java/org/financeapp/iam/domain/model/aggregates/User.java
@@ -4,58 +4,44 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
-import org.financeapp.iam.domain.model.entities.Role;
+import org.financeapp.iam.domain.model.valueobjects.Roles;
 import org.financeapp.shared.model.aggregates.AuditableAbstractAggregateRoot;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
-@Setter
 @Entity
 public class User extends AuditableAbstractAggregateRoot<User> {
 
     @NotBlank
     @Size(min = 4, max = 20)
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String username;
 
     @NotBlank
     @Size(min = 8, max = 100)
+    @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Roles> roles = new HashSet<>();
 
-    public User() {
-        this.roles = new HashSet<>();
+    protected User() {
+        // Required by JPA
     }
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.roles = new HashSet<>();
+        this.roles.add(Roles.ROLE_USER);
     }
 
-    public User(String username, String password, List<Role> roles) {
-        this(username, password);
-        addRoles(roles);
+    public Set<Roles> getRoles() {
+        return Set.copyOf(roles); // Inmutabilidad externa
     }
-
-    public User addRole(Role role){
-        this.roles.add(role);
-        return this;
-    }
-
-    public User addRoles(List<Role> roles){
-        var validatedRoles = Role.validateRoleSet(roles);
-        this.roles.addAll(validatedRoles);
-        return this;
-    }
-
 }
+
